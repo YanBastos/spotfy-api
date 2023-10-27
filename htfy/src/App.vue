d041650c9ed48008e2cfd391078d7c7713db489f
YanBastos <yanmoraesbastos@hotmail.com>
2023-10-26 21:15:50 -0300

    commit

--- htfy/src/App.vue
+++ htfy/src/App.vue
@@ -1,5 +1,37 @@
 <template>
-  <section class="w-2/5 bg-green-500 absolute left-0 top-0 h-full"></section>
-  <p>asuhdaui</p>
-  <section></section>
+  <section class="w-2/5 bg-green-500 absolute left-0 top-0 h-full p-20">
+    <h2 class="text-white text-4xl font-bold mt-20">
+      Entre agora para começar a curtir as suas músicas preferidas
+    </h2>
+  </section>
+
+  <section id="login" class="w-3/5 absolute right-0 top-0 h-full">
+    <div class="p-20">
+      <img class="m-auto w-50" src="./assets/logo.png" />
+    </div>
+    <div class="text-center w-full absolute bottom-10">
+      <button
+        class="bg-green-500 text-white px-10 py-5 text-xl block m-auto rounded-full"
+      >
+        Entrar com Spotfy
+      </button>
+    </div>
+  </section>
 </template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      clientID: "2092b9ea3bbb4a07bd382a633eb8ce14",
+      scopes: "user-read-private user-read-email",
+      redirectURI: "http://localhost:5173/playlists",
+    }
+  },
+  methods: {
+    login() {
+      location.href= `https://accounts.spotify.com/authorize?response_type=code&client_id=${this.clientID}&scope=${this.scopes}&redirect_uri${redirectURI}`
+    }
+  },
+};
+</script>
--- htfy/src/main.js
+++ htfy/src/main.js
@@ -1,5 +1,27 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
+// import express, { query } from 'express';
+
+
+// var client_id = '2092b9ea3bbb4a07bd382a633eb8ce14';
+// var redirect_uri = 'http://localhost:5173/playlists'
+
+// var app = express();
+
+// App.get("/login", function (req, res) {
+//     var state = generateRamdomString(16);
+//     var scope = 'user-read-private user-read-email';
+
+//     res.redirect('https://accounts.spotfy.com/authorize?' +
+//     querystrin.stringfy({
+//         response_type: 'code',
+//         client_id: client_id,
+//         scope: scope,
+//         redirect_uri:redirect_uri,
+//         state: state
+//     }));
+// });
+
 
 createApp(App).mount('#app') //index.html div "id=app"
